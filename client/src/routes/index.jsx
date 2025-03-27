410660ef39a99513159a1033fcc5e9665facd3a4
Rishav Raj <rajrishav011@gmail.com>
2025-03-28 01:17:04 +0530

    update some routes

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,13 +2,16 @@ import {Navigate , useRoutes} from 'react-router-dom'
 
 import NavLayout from '../layout/navLayout'
 import Main from '../pages/main/Main'
-import Mern_Highlights from '../pages/navPages/Mern_HighlightsPage'
-import Pern_Highlights from '../pages/navPages/Pern_HighlightsPage'
-import Develop_Service from '../pages/navPages/Develop_ServicePage'
-import Design_Service from '../pages/navPages/DesignServicePage'
-import AssignProject from '../pages/navPages/AssignProjectPage'
-import About from '../pages/navPages/AboutPage'
-import Contact from '../pages/navPages/ContactPage'
+import Mern_Highlights from '../pages/ScreenNav/Mern_HighlightsPage'
+import Pern_Highlights from '../pages/ScreenNav/Pern_HighlightsPage'
+import Develop_Service from '../pages/ScreenNav/Develop_ServicePage'
+import Design_Service from '../pages/ScreenNav/DesignServicePage'
+import AssignProject from '../pages/ScreenNav/AssignProjectPage'
+import About from '../pages/ScreenNav/AboutPage'
+import Contact from '../pages/ScreenNav/ContactPage'
+import Labs from '../pages/ScreenNav/LabsPage'
+import Mobile_Highlights from '../pages/MobileNav/HighlightsPage'
+import Mobile_Service from '../pages/MobileNav/ServicesPage';
 
 export default function Router(){
     return useRoutes([
@@ -24,7 +27,10 @@ export default function Router(){
                 {path: 'pern-highlights',element:<Pern_Highlights/>},
                 {path: 'about',element:<About/>},
                 {path: 'Contact',element:<Contact/>},
+                {path: 'tech_lab',element:<Labs/>},
                 {path: 'assign-project',element:<AssignProject/>},
+                {path: 'highlights',element:<Mobile_Highlights/>},
+                {path: 'services',element:<Mobile_Service/>},
                 { path: "*", element: <Navigate to="/404" replace /> }
             ]
         }
