32b9d4738db754851327f1d76e10ef072b217210
Rishav Raj <rajrishav011@gmail.com>
2025-03-29 23:06:29 +0530

    add design and develop data

---
+++ client/src/components/Data/Develop_DesignData.js
@@ -0,0 +1,91 @@
+export const develop =[
+    {
+        id:1,
+        service:'Frontend Development'
+    },
+    {
+        id:2,
+        service:'Backend Development'
+    },
+    {
+        id:3,
+        service:'Database Management'
+    },
+    {
+        id:4,
+        service:'Deployment'
+    },
+    {
+        id:5,
+        service:'Consultation Services'
+    },
+    
+
+]
+
+export const support =[
+    {
+        id:1,
+        service:'Web Hosting'
+    },
+    {
+        id:2,
+        service:'Bug fixing'
+    },
+    {
+        id:3,
+        service:'Performance optimization '
+    },
+    {
+        id:4,
+        service:'Application updates'
+    },
+    {
+        id:5,
+        service:'Site Monitoring'
+    },
+    
+]
+
+export const design = [
+    {
+        id:1,
+        service:'Custom Web Design'
+    },
+    {
+        id:2,
+        service:'UI/UX Design'
+    },
+    {
+        id:3,
+        service:'E-Commerce Design'
+    },
+    {
+        id:4,
+        service:'Responsive Design'
+    },
+    
+]
+
+export const other  = [
+    {
+        id:1,
+        service:'Design Updates'
+    },
+    {
+        id:2,
+        service:'Optimization'
+    },
+    {
+        id:3,
+        service:'SEO-Friendly Design'
+    },
+    {
+        id:4,
+        service:'Content Layout'
+    },
+    {
+        id:5,
+        service:'Web Strategy'
+    },
+]
