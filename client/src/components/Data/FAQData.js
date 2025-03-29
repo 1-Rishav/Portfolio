9730c251142fe2819df5d3d88ab6b2973e720039
Rishav Raj <rajrishav011@gmail.com>
2025-03-29 23:06:49 +0530

    add faq data

---
+++ client/src/components/Data/FAQData.js
@@ -0,0 +1,33 @@
+export const faq =[
+    {
+        id:1,
+        title:'What services do you offer as a web developer?',
+        answer:'I provide end-to-end web development services, including frontend and backend development, UI/UX design, responsive design, e-commerce development, web application creation, and website maintenance.'
+    },
+    {
+        id:2,
+        title:'How long will it take to build website?',
+        answer:'The timeline depends on the complexity and scope of the project. A basic website may take 4–5 weeks, while a more complex application could take several months. I will provide an estimated timeline after discussing your requirements.'
+    },
+    {
+        id:3,
+        title:'How much does a website cost?',
+        answer:"I can't say exactly it depends on various factors, including its complexity, features, design requirements, and functionality.After discussing your needs and goals, I’ll provide a detailed quote tailored to your project. Rest assured, I aim to deliver value that aligns with your budget. "
+    },
+    {
+        id:4,
+        title:'What are your payment requirements?',
+        answer:"I typically require an upfront deposit of 40% to secure the project,other 30% at mid stage of project and remaining amount after completion of project, but don't worry I am flexible. We can discuss about it ."
+    },
+    {
+        id:5,
+        title:'How many meetings can we have?',
+        answer:"As many meetings as you need! Whether it’s brainstorming ideas, discussing progress,etc.I’m here to ensure everything aligns with your vision.We can connect via Google Meet or Zoom,My goal is to guide you with the right suggestions while making you feel comfortable throughout the process. "
+    },
+    {
+        id:6,
+        title:'What if I don’t have a design or content for the website?',
+        answer:'No problem! I can help with UI/UX design and work with you to create content or integrate content from a other service.'
+    },
+   
+] 
