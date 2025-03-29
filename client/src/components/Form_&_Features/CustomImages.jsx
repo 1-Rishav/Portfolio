5337d7d597e58bd26f575fe6e6f4c8692543f28f
Rishav Raj <rajrishav011@gmail.com>
2025-03-29 23:09:42 +0530

    add customizable images

---
+++ client/src/components/Form_&_Features/CustomImages.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function CustomImages({img1 , img2 , img3 , img4 , img5 , img6 , img7 , img8,img9,img10,img11,img12}) {
+  return (
+    <>
+    
+           
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img1} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img2} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img3} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img4} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img5} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img6} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img7} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img8} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img9} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img10} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img11} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            <div className='h-full w-full flex items-center justify-center '>
+              <img src={img12} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+            </div>
+            
+            
+            
+        
+    </>
+  )
+}
+
+export default CustomImages
