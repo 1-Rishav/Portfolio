7510417c85069900e2de1e60a27cd6373e23db45
Rishav Raj <rajrishav011@gmail.com>
2025-03-29 23:07:19 +0530

    add pern and mern project data

---
+++ client/src/components/Data/HighlightData.js
@@ -0,0 +1,53 @@
+export const mernHighlight=[
+    {
+        id:1,
+        feature:'AI Chat'
+    },
+    {
+        id:2,
+        feature:'Voice Call'
+    },
+    {
+        id:3,
+        feature:'Video Call'
+    },
+    {
+        id:4,
+        feature:'Travel Genni'
+    },
+    {
+        id:5,
+        feature:'Seamless Experience'
+    }
+]
+
+export const pernHighlight=[
+    {
+        id:1,
+        feature:'Investment'
+    },
+    {
+        id:2,
+        feature:'Borrowing '
+    },
+    {
+        id:3,
+        feature:'Negotiation'
+    },
+    {
+        id:4,
+        feature:'Document Uploads'
+    },
+    {
+        id:5,
+        feature:'Email Verification'
+    },
+    {
+        id:6,
+        feature:'Loan Payment'
+    },
+    {
+        id:7,
+        feature:'Loan Repayment'
+    },
+]
