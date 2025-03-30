17e70d26a313256a7f022ebfbd3e328f16500e40
Rishav Raj <rajrishav011@gmail.com>
2025-03-30 23:50:51 +0530

    add design page

---
+++ client/src/pages/ScreenNav/DesignServicePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Design_Service from '../../components/Navigation/Design_Service'
+import { BackgroundBeamsWithCollisionDemo } from '../../components/Footer_BeamsCollision'
+import { NavbarDemo } from '../../components/Floating_Nav'
+
+function DesignServicePage() {
+  return (
+    <>
+    <NavbarDemo/>
+    <Design_Service/>
+    <BackgroundBeamsWithCollisionDemo/>
+    </>
+)
+}
+
+export default DesignServicePage
