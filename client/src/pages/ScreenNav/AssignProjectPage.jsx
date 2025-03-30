fa2d905f31de43fc468044e9d013794ad624799b
Rishav Raj <rajrishav011@gmail.com>
2025-03-30 23:50:02 +0530

    add assign_project page

---
+++ client/src/pages/ScreenNav/AssignProjectPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AssignProject from '../../components/Navigation/AssignProject'
+import { NavbarDemo } from '../../components/Floating_Nav'
+import { BackgroundBeamsWithCollisionDemo } from '../../components/Footer_BeamsCollision'
+
+function AssignProjectPage() {
+  return (
+    <>
+    <NavbarDemo/>
+    <AssignProject/>
+    <BackgroundBeamsWithCollisionDemo/>
+    </>
+  )
+}
+
+export default AssignProjectPage
