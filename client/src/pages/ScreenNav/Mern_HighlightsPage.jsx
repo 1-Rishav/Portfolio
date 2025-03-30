7c90619067e4a7156921ab241a1df8c1df921f14
Rishav Raj <rajrishav011@gmail.com>
2025-03-30 23:52:23 +0530

    add mern project page

---
+++ client/src/pages/ScreenNav/Mern_HighlightsPage.jsx
@@ -0,0 +1,17 @@
+
+import { NavbarDemo } from '../../components/Floating_Nav'
+import { BackgroundBeamsWithCollisionDemo } from '../../components/Footer_BeamsCollision'
+import Mern_Highlights from '../../components/Navigation/Mern_Highlights'
+import React from 'react'
+
+function Mern_HighlightsPage() {
+  return (
+    <>
+    <NavbarDemo/>
+    <Mern_Highlights/>
+    <BackgroundBeamsWithCollisionDemo/>
+    </>
+  )
+}
+
+export default Mern_HighlightsPage
