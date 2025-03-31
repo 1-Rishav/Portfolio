edcf76f0c139b49e18b8b37e8092a84c2ee8daf1
Rishav Raj <rajrishav011@gmail.com>
2025-03-31 23:58:48 +0530

    component for uploading project docs

---
+++ client/src/components/File_upload.jsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { useState } from "react";
+import { FileUpload } from "./ui/file_upload";
+
+export function FileUploadDemo({selectedFile}) {
+  const [files, setFiles] = useState([]);
+  const handleFileUpload = async(files) => {
+   
+      setFiles(files);
+      selectedFile(files);
+  };
+
+  return (
+    (<div
+      className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-emerald-500 dark:border-neutral-800 rounded-lg">
+      <FileUpload onChange={handleFileUpload} />
+    </div>)
+  );
+}
