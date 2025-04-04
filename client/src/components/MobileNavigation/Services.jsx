933fc5892b3720f810a028a3013be11af99b8762
Rishav Raj <rajrishav011@gmail.com>
2025-04-04 23:31:33 +0530

    development and design view in mobile

---
+++ client/src/components/MobileNavigation/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Develop_Service from '../Navigation/Develop_Service'
+import Design_Service from '../Navigation/Design_Service'
+
+const Services = () => {
+  return (
+    <>
+    <Develop_Service/>
+    <Design_Service/>
+    </>
+  )
+}
+
+export default Services
