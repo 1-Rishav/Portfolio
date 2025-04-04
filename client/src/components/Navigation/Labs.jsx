7788234468e8a16a23b5a5fc8d3d3bc1307f6f1c
Rishav Raj <rajrishav011@gmail.com>
2025-04-04 23:28:09 +0530

    tech lab view soon

---
+++ client/src/components/Navigation/Labs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { TextHoverEffect } from "../ui/text-hover-effect";
+
+const Labs = () => {
+  return (
+    <>
+  
+    <div className=" min-h-screen h-full flex items-center justify-center ">
+      <TextHoverEffect text="Comming Soon" />
+    </div>
+
+    </>
+  )
+}
+
+export default Labs
