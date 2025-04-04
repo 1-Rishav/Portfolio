f65cb32569165640df93ce2036cca135398eadb9
Rishav Raj <rajrishav011@gmail.com>
2025-04-04 23:30:41 +0530

    mobile view on my developed project

---
+++ client/src/components/MobileNavigation/Highlights.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Mern_Highlights from '../Navigation/Mern_Highlights'
+import Pern_Highlights from '../Navigation/Pern_Highlights'
+
+const Highlights = () => {
+  return (
+    <>
+    <Mern_Highlights/>
+    <Pern_Highlights/>
+    </>
+  )
+}
+
+export default Highlights
