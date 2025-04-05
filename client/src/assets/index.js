313a2c994ab7f204215941a67453b14866f5032f
Rishav Raj <rajrishav011@gmail.com>
2025-04-05 23:18:34 +0530

    main gate for all images

---
+++ client/src/assets/index.js
@@ -0,0 +1,60 @@
+import Project1 from './Images/Project1.jpg'
+import Project1_i from './Images/Project1_i.png'
+import Project2 from './Images/Project2.png'
+import Project3 from './Images/Project3.jpg'
+import Project4 from './Images/Project4.jpg'
+import Project5 from './Images/Project5.jpg'
+import Project6 from './Images/Project6.jpg'
+import Project7 from './Images/Project7.jpg'
+import Project8 from './Images/Project8.jpg'
+import Project9 from './Images/Project9.jpg'
+import Project10 from './Images/Project10.jpg'
+import GemXVideo from './ProjectVideos/GemX.mp4'
+import QuickFundzVideo from './ProjectVideos/QuickFundz.mp4'
+import AdvertiseVideo from './ProjectVideos/Advertise.mp4'
+import AboutVideo from './ProjectVideos/About .mp4';
+import Rishav1 from './Images/Rishav1.jpg'
+import Rishav2 from './Images/Rishav2.jpg'
+import Rishav3 from './Images/Rishav3.jpg'
+import Rishav4 from './Images/Rishav4.jpg'
+import Rishav5 from './Images/Rishav5.jpg'
+import Rishav6 from './Images/Rishav6.jpg'
+import Quick_Logo from './Images/Quickfundz_logo.jpg'
+import GemX_Logo from './Images/GemX_logo.png'
+import program1 from './ProgrammingImages/LeetCode.png'
+import program2 from './ProgrammingImages/geek.png'
+import program3 from './ProgrammingImages/HackerRank.jpg'
+import program4 from './ProgrammingImages/CodeChef.jpg'
+import program5 from './ProgrammingImages/CodingNinja.webp'
+
+import Gem1 from './GemXImages/Image1.png';
+import Gem2 from './GemXImages/Image2.png';
+import Gem3 from './GemXImages/Image3.png';
+import Gem4 from './GemXImages/Image4.png';
+import Gem5 from './GemXImages/Image5.png';
+import Gem6 from './GemXImages/Image6.png';
+import Gem7 from './GemXImages/Image7.png';
+import Gem8 from './GemXImages/Image8.png';
+
+import Quick1 from './QuickFundzImages/Image1.jpg'
+import Quick2 from './QuickFundzImages/Image2.jpg'
+import Quick3 from './QuickFundzImages/Image3.jpg'
+import Quick4 from './QuickFundzImages/Image4.jpg'
+import Quick5 from './QuickFundzImages/Image5.jpg'
+import Quick6 from './QuickFundzImages/Image6.jpg'
+
+import Skill1 from './Skills/Skill1.png'
+import Skill2 from './Skills/Skill2.png'
+import Skill3 from './Skills/Skill3.png'
+import Skill4 from './Skills/Skill4.png'
+import Skill5 from './Skills/Skill5.png'
+import Skill6 from './Skills/Skill6.png'
+import Skill7 from './Skills/Skill7.png'
+import Skill8 from './Skills/Skill8.png'
+import Skill9 from './Skills/Skill9.png'
+import Skill10 from './Skills/Skill10.png'
+import Skill11 from './Skills/Skill11.png'
+import Skill12 from './Skills/Skill12.png'
+
+
+export default {Project1, Project2, Project3,Project1_i , Quick_Logo , GemX_Logo , Project4, Project5, Project6, Project7, Project8, Project9 ,Project10 ,GemXVideo,QuickFundzVideo,AdvertiseVideo,AboutVideo, Rishav1, Rishav2, Rishav3,Rishav4, Rishav5 ,Rishav6,program1,program2,program3,program4,program5,Gem1,Gem2,Gem3,Gem4,Gem5,Gem6,Gem7,Gem8,Quick1,Quick2,Quick3,Quick4,Quick5,Quick6,Skill1,Skill2,Skill3,Skill4,Skill5,Skill6,Skill7,Skill8,Skill9,Skill10,Skill11,Skill12}
