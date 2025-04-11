39b004568283bc0333ec3d6747eebb353501c32f
Rishav Raj <rajrishav011@gmail.com>
2025-04-11 22:59:09 +0530

    add loading screen

---
+++ client/src/components/Form_&_Features/LoadingHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoadingHome = () => {
+  return (
+    
+      <div className="fixed top-0 left-0 w-full h-1 bg-transparent z-50">
+        <div className="h-full bg-gradient-to-r from-emerald-500 via-purple-500 via-emerald-500 to-cyan-500 animate-loadbar rounded-full" />
+      </div>
+    
+  )
+}
+
+export default LoadingHome
