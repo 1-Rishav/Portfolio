d86b7ff6a32b6498bdcfc1a4e86fcc50da9a2928
Rishav Raj <rajrishav011@gmail.com>
2025-04-13 22:08:00 +0530

    configure toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 //import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <ToastContainer
+              position="bottom-center"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              theme="dark"
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              />
     <App />
     </BrowserRouter>
   </StrictMode>,
