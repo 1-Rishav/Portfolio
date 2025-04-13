0a6d75866b74fba8e3c8d13789006c316558252c
Rishav Raj <rajrishav011@gmail.com>
2025-04-13 13:58:21 +0530

    customize usable component

--- client/src/components/Form_&_Features/CustomImages.jsx
+++ client/src/components/Form_&_Features/CustomImages.jsx
@@ -1,46 +1,14 @@
 import React from 'react'
 
-function CustomImages({img1 , img2 , img3 , img4 , img5 , img6 , img7 , img8,img9,img10,img11,img12}) {
+function CustomImages({images=[]}) {
   return (
     <>
     
-           
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img1} alt="" className=' object-cover rounded-xl h-full w-60 '/>
+    {images.map((img, index) => (
+            <div key={index} className='h-full w-full flex items-center justify-center '>
+              <img src={img} alt={`custom-img-${index}`} className=' object-cover rounded-xl h-full w-60 '/>
             </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img2} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img3} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img4} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img5} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img6} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img7} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img8} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img9} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img10} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img11} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            <div className='h-full w-full flex items-center justify-center '>
-              <img src={img12} alt="" className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
+            ))}
             
             
             
