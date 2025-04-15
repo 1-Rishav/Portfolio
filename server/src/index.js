c45f74ebbc9c92fe494a656db2c11f79679f8388
Rishav Raj <rajrishav011@gmail.com>
2025-04-16 00:46:47 +0530

    function to awake server

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,15 @@ app.get('/',(req, res) =>{
     res.send("Never Ever GiveUp!");
 })
 
+const axios = require('axios');
+const url = 'https://portfolio-z306.onrender.com'; 
+setInterval(() => {
+  axios.get(url)
+    .then(() => console.log('Stay awake'))
+    .catch(err => console.error('Going to down!', err));
+}, 14 * 60 * 1000); // Every 14 minutes
+
+
 connectDB().then(()=>{
     app.listen(port,()=>{
         console.log(`App running on port ${port} ...`)
