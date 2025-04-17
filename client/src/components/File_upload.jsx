c7f579b68715b297c05c0bc9e5f0dea7f038177c
Rishav Raj <rajrishav011@gmail.com>
2025-04-17 09:07:43 +0530

    passing to props value to clear file after submission

--- client/src/components/File_upload.jsx
+++ client/src/components/File_upload.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { FileUpload } from "./ui/file_upload";
 
-export function FileUploadDemo({selectedFile}) {
+export function FileUploadDemo({selectedFile , setValue}) {
   const [files, setFiles] = useState([]);
   const handleFileUpload = async(files) => {
    
@@ -13,7 +13,7 @@ export function FileUploadDemo({selectedFile}) {
   return (
     (<div
       className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-emerald-500 dark:border-neutral-800 rounded-lg">
-      <FileUpload onChange={handleFileUpload} />
+      <FileUpload onChange={handleFileUpload} value={setValue}/>
     </div>)
   );
 }
