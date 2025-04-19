c1626b9465d12b9d48cfa7e734de69cc3d65c771
Rishav Raj <rajrishav011@gmail.com>
2025-04-19 19:24:31 +0530

    connection retrieve route

--- server/src/routes/contact.route.js
+++ server/src/routes/contact.route.js
@@ -3,5 +3,6 @@ const contactController = require('../controller/contactController')
 
 //router.post('/home',contactController.contactHome);
 router.post('/contactPage',contactController.contactPage);
+router.get('/connections',contactController.allConnection);
 
 module.exports = router;
