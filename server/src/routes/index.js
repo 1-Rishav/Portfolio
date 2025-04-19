3410af295e9d3503fbe3c827cea3e8b95b5563c9
Rishav Raj <rajrishav011@gmail.com>
2025-04-19 19:25:25 +0530

    add route path and all projects retrieve route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const contact = require('./contact.route')
 const project = require('./project.route')
+const User = require('./user.route')
 
 router.use('/contact',contact);
 router.use('/project',project);
+router.use('/auth',User);
 
 module.exports = router;
--- server/src/routes/project.route.js
+++ server/src/routes/project.route.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const projectController = require('../controller/projectController')
 const {upload} = require('../middleware/cloudinary.middleware')
 router.post('/new-project',upload.single('file'),projectController.newProject)
+router.get('/projects',projectController.allAssignedProject)
 
 module.exports = router;
