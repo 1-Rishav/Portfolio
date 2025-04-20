1a7b157b0fc50d439403d804517a1b817d4e0810
Rishav Raj <rajrishav011@gmail.com>
2025-04-20 23:52:33 +0530

    loading animation and some bug fixed

--- client/src/components/Form_&_Features/ContactForm.jsx
+++ client/src/components/Form_&_Features/ContactForm.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { GoArrowUpRight } from "react-icons/go";
 import axios from "../../utils/axios";
 import { toast } from "react-toastify";
+import LoadingScreen from "../LoadingScreen";
 
 function ContactForm() {
   const [enteredValue , setEnteredValue] =useState({
@@ -15,7 +16,7 @@ function ContactForm() {
 
   })
   const [error , setError] = useState(true)
-
+  const [loading , setLoading] = useState(false)
 
   const handleChange = (name,e)=>{
     setEnteredValue((prevValue)=>{
@@ -46,9 +47,11 @@ function ContactForm() {
     }
     
     try {
+      setLoading(true)
       const response = await axios.post('contact/contactPage',data,{ withCredentials:true});
  
       toast.success(response.data.message)
+      setLoading(false)
     } catch (error) {
       toast.error(error.message)
     }
@@ -91,7 +94,7 @@ function ContactForm() {
               <Textarea label="Description" variant="faded" placeholder="Describe your query" value={enteredValue.describe} onChange={(e)=>handleChange('describe',e)}  isRequired/>
             </div>
             <button type="submit" disabled={error} onClick={handleSubmit}  className="  inline-flex relative group outline-none  | focus:outline-none "><div className={`w-auto ${error? 'bg-gray-400 cursor-not-allowed':'bg-emerald-300'} inline-flex items-center justify-center relative leading-tight shadow-none overflow-hidden rounded-full border-default text-black py-2 px-5`}><div className={`relative inline-flex items-center justify-center top-px flex-shrink-0  ${error? 'bg-gray-400 cursor-not-allowed':'bg-emerald-300'}`}><div>
-                Ready to connect</div></div></div><div className={`${error? 'bg-gray-400 cursor-not-allowed':'bg-emerald-300'} flex-shrink-0 overflow-hidden flex items-center justify-center -ml-1 rounded-full transform transition-transform | w-9 h-9 | xl:group-hover:translate-x-3  xl:group-hover:rotate-45 | js-button-icon`}><GoArrowUpRight /></div></button>
+              {loading ? <LoadingScreen/> : "Ready to connect"}</div></div></div><div className={`${error? 'bg-gray-400 cursor-not-allowed':'bg-emerald-300'} flex-shrink-0 overflow-hidden flex items-center justify-center -ml-1 rounded-full transform transition-transform | w-9 h-9 | xl:group-hover:translate-x-3  xl:group-hover:rotate-45 | js-button-icon`}><GoArrowUpRight /></div></button>
           </div>
 
         </div>
