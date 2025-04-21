f6b7b10ad358ffebad75a858506b64a25b63eb1c
Rishav Raj <rajrishav011@gmail.com>
2025-04-21 23:40:48 +0530

    admin navigation

---
+++ client/src/pages/AdminNav/AssignedProjectsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AssignedProjects from '../../components/admin/AssignedProjects'
+
+const AssignedProjectsPage = () => {
+  return (
+    <>
+    <AssignedProjects/>
+    </>
+  )
+}
+
+export default AssignedProjectsPage
---
+++ client/src/pages/AdminNav/ConnectionsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Connections from '../../components/admin/Connections'
+
+const ConnectionsPage = () => {
+  return (
+
+    <>
+    <Connections/>
+    </>
+)
+}
+
+export default ConnectionsPage
