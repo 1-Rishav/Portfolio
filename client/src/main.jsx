90ed41d6fee5c0e3968010d6937bff6d469eb029
Rishav Raj <rajrishav011@gmail.com>
2025-04-21 23:44:01 +0530

    update main file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import {Provider} from 'react-redux'
+import { store,persistor} from './store/index.js';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'react-toastify/dist/ReactToastify.css';
 //import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
     <ToastContainer
               position="bottom-center"
@@ -23,5 +28,7 @@ createRoot(document.getElementById('root')).render(
               />
     <App />
     </BrowserRouter>
-  </StrictMode>,
+    </PersistGate>
+    </Provider>
+  </StrictMode>
 )
