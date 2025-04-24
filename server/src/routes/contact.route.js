0bdf7ee12fc98935077220730d1e0161daf99deb
Rishav Raj <rajrishav011@gmail.com>
2025-04-24 16:48:42 +0530

    create route to check connectionStatus

--- server/src/routes/contact.route.js
+++ server/src/routes/contact.route.js
@@ -4,5 +4,6 @@ const contactController = require('../controller/contactController')
 //router.post('/home',contactController.contactHome);
 router.post('/contactPage',contactController.contactPage);
 router.get('/connections',contactController.allConnection);
+router.post('/checkConnection',contactController.connected)
 
 module.exports = router;
