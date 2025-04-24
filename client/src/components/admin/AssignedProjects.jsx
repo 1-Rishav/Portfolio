c2de896a8d5c09ca9284f3bc0e0a662725091838
Rishav Raj <rajrishav011@gmail.com>
2025-04-24 16:50:50 +0530

    showing connections and projectdetail to admin

--- client/src/components/admin/AssignedProjects.jsx
+++ client/src/components/admin/AssignedProjects.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../../utils/axios';
+import { toast } from 'react-toastify';
+import { MeteorAssignedProject } from '../Meteor_AssignedProject';
+import { TextHoverEffect } from "../ui/text-hover-effect";
 
 const AssignedProjects = () => {
-  return (
-    <div>AssignedProjects</div>
-  )
+  const [assignedProject , setAssignedProject]=useState({projects:[]});
+  
+    useEffect(()=>{
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('/project/projects');
+          const data = await response.data;
+          setAssignedProject(data);
+        } catch (error) {
+          toast.error(error.message);
+        }
+      };
+  
+      fetchData();
+    },[])
+  
+    return (
+      <>
+      
+      <div className='flex items-center justify-center min-h-screen h-full w-full  transform  bg-red-200 bg-gradient-to-r from-blue-100 to-teal-100 overflow-y-scroll '>
+        {assignedProject?.projects?.length==0? 
+            <div className='flex  flex-wrap gap-6 justify-around h-full min-h-screen w-full md:w-[70%]   bg-neutral-100 rounded-xl'><TextHoverEffect text="No Projects" /></div>:<div className='flex  flex-wrap gap-6 justify-around h-full min-h-screen w-full md:w-[70%]   bg-neutral-100 rounded-xl '>
+            { assignedProject?.projects?.map((data)=>(
+           <div key={data._id} >
+            
+               <MeteorAssignedProject name={data.name} number={data.phone} company={data.business} email={data.email} description={data.describe} file={data.file} id={data._id} checked={data.view}/>
+               
+        </div>
+          ))}  
+          
+           
+           </div>}
+        
+      </div>
+      </>
+    )
 }
 
 export default AssignedProjects
--- client/src/components/admin/Connections.jsx
+++ client/src/components/admin/Connections.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { MeteorsDemo } from '../Meteor_Box'
+import axios from '../../utils/axios';
+import { toast } from 'react-toastify';
+import { TextHoverEffect } from "../ui/text-hover-effect";
+
 
 const Connections = () => {
+  const [connectionData , setConnectionData]=useState({user:[]});
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/contact/connections');
+        const data = await response.data;
+        setConnectionData(data);
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+
+    fetchData();
+  },[])
+
+
   return (
-    <div>Connections</div>
+    <>
+    
+    <div className='flex items-center justify-center min-h-screen h-full w-full  transform  bg-red-200 bg-gradient-to-r from-blue-100 to-teal-100 overflow-y-scroll '>
+    {connectionData?.user?.length==0? 
+    <div className='flex  flex-wrap gap-6 justify-around h-full min-h-screen w-full md:w-[70%]   bg-neutral-100 rounded-xl'><TextHoverEffect text="No Connections" /></div>:<div className='flex  flex-wrap gap-6 justify-around h-full min-h-screen w-full md:w-[70%]   bg-neutral-100 rounded-xl '>
+       { connectionData?.user?.map((data)=>(
+      <div key={data._id} >
+       
+          <MeteorsDemo name={data.name} number={data.phone} company={data.business} email={data.email} description={data.describe} id={data._id} checked={data.view}/>
+        
+     
+   </div>
+     ))}  
+     
+      
+      </div>}
+      
+    </div>
+    </>
   )
 }
 
