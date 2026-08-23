0e0cae83045530a9b201fcde096ae326963daeb5
Rishav <rajrishav011@gmail.com>
2026-08-23 15:23:32 +0530

    modify app on client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import './App.css'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 import Router from './routes/index';
+import { CheckAuth } from './store/slices/authSlice';
+
 const App=()=> {
- 
+  const dispatch = useDispatch();
+
+  // Persisted (localStorage) isLoggedIn/role is only ever a UI hint - this
+  // confirms it against the real session cookie once on load and corrects
+  // it if the cookie is missing/expired.
+  useEffect(() => {
+    dispatch(CheckAuth());
+  }, [dispatch]);
+
   return (
     <>
       
