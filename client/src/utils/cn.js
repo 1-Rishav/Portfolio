df438dfd288a4c182c881c7a2592a160330f0881
Rishav <rajrishav011@gmail.com>
2026-08-27 21:57:18 +0530

    remove duplicate file

--- client/src/utils/cn.js
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-import '../App.css'
-//import '../index.css'; // Make sure to import your Tailwind CSS file
-
-export function cn(...inputs) {
-  return twMerge(clsx(...inputs));
-}
