f7b6acd9cef02654adea62669ed0229222f9023d
Rishav <rajrishav011@gmail.com>
2026-09-26 12:50:16 +0530

    update customimages file to correct horizontal scroll

--- client/src/components/Form_&_Features/CustomImages.jsx
+++ client/src/components/Form_&_Features/CustomImages.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React from "react";
 
-function CustomImages({images=[]}) {
+function CustomImages({ images = [] }) {
   return (
     <>
-    
-    {images.map((img, index) => (
-            <div key={index} className='h-full w-full flex items-center justify-center '>
-              <img src={img} alt={`custom-img-${index}`} className=' object-cover rounded-xl h-full w-60 '/>
-            </div>
-            ))}
-            
-            
-            
-        
+      {images.map((img, index) => (
+        <div
+          key={`${index}-${img}`}
+          className="
+            w-[180px]
+            sm:w-[200px]
+            lg:w-[280px]
+            xl:w-[300px]
+            aspect-[16/9]
+            shrink-0
+            flex-none
+            overflow-hidden
+            rounded-xl
+            bg-black
+          "
+        >
+          <img
+            src={img}
+            alt={`custom-img-${index}`}
+            draggable={false}
+            className="w-full h-full object-cover block rounded-xl"
+          />
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default CustomImages
+export default CustomImages;
